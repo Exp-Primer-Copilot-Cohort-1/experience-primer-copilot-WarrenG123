d7bd7eb19cd919e969fbc7068dd08ae0a7b5916a
Waguti Warren <warrenw639@gmail.com>
2024-01-23 14:59:59 +0000

    Copilot third commit

--- comment.js
+++ comment.js
@@ -1,23 +1,42 @@
-// Create web server with express
-// Create router for comment
-// Create controller for comment
-// Create model for comment
-// Create view for comment
-// Create database for comment
-// Create API for comment
+//create web server
+//1. create server
+//2. create router
+//3. set router
+//4. start server
 
-// Import module
-const express = require('express');
-const router = express.Router();
-const commentController = require('../controllers/commentController');
+//1. create server
+var http = require('http');
+var express = require('express');
+var app = express();
+var server = http.createServer(app);
+var path = require('path');
+var bodyParser = require('body-parser');
+var comment = require('./comment');
 
-// Create router
-router.get('/', commentController.index);
-router.get('/create', commentController.create);
-router.post('/store', commentController.store);
-router.get('/:id/edit', commentController.edit);
-router.put('/:id', commentController.update);
-router.delete('/:id', commentController.destroy);
+//2. create router
+var router = require('./router/main')(app, comment);
 
-// Export router
-module.exports = router;
+//3. set router
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile); //html template engine
+
+//4. start server
+server.listen(3000, function(){
+    console.log('Express server has started on port 3000');
+});
+
+//5. set static file
+app.use(express.static('public'));
+
+//6. use body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+//7. use session
+var session = require('express-session');
+app.use(session({
+    secret: '@#@$MYSIGN#@$#$',
+    resave: false,
+    saveUninitialized: true
+}));
